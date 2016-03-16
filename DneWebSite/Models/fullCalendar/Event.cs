2f916cfc3c0c1772879fa5c944b0bb6319981497
yiren <joombuopre@outlook.com>
2016-03-16 10:27:46 +0800

    Update Link

---
+++ DneWebSite/Models/fullCalendar/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DneWebSite.Models.fullCalendar
+{
+    class Event
+    {   
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Location { get; set; }
+        public string Attendee { get; set; }
+    }
+}
