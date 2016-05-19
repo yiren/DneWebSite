8a825ad6a553942a7bf1e19aad8b56370a23452e
yiren <joombuopre@outlook.com>
2016-05-19 17:13:41 +0800

    RC 2

--- DneWebSite/client/controllers/homeController.js
+++ DneWebSite/client/controllers/homeController.js
@@ -2,15 +2,15 @@
 	'use strict';
 
 	angular.module('main')
-		   .controller('homeCtrl', ['$log','$state', function ($log, $state) {
-		       var vm = this;
-                //舊網主機位址
-		   		vm.host = '10.144.101.3';
-		   		vm.host2 = '10.144.101.5';
-		   		vm.goToHome = function () {
-		   		    //ui-router $state.go可以跳到router.js定義的state路徑名稱
-		   		    $state.go('home');
-		   		}
+        .controller('homeCtrl', ['$log', '$state', function ($log, $state) {
+            var vm = this;
+            //舊網主機位址
+            vm.host = '10.144.101.3';
+            vm.host2 = '10.144.101.5';
+            vm.goToHome = function () {
+		   	    //ui-router $state.go可以跳到router.js定義的state路徑名稱
+		   	    $state.go('home');
+		   	}
 		   }])
 
 })();
