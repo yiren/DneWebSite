f365164494d2428dd58d3f42b559890f45a2f2a3
yiren <joombuopre@outlook.com>
2016-06-03 08:53:48 +0800

    淑娟姐要求新增tooltip於公文查詢系統連結

---
+++ DneWebSite/js/qTip2.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+    $('#newDoc').qtip({
+        content: '<h4>英文(2007~迄今)</h4>',
+        position: {
+            my: 'left center',
+            at: 'right center',
+            target:$('#newDoc')
+        },
+        show: {
+            effect: function (offset) {
+                $(this).slideDown(100);
+            }
+        },
+        style:{
+            classes:'qtip-green qtip-rounded'
+        }
+    });
+    $('#oldDoc').qtip({
+        content: '<ul class="list"><li><h4>中文(1998~迄今)</h4></li><li><h4>英文(1998~2007)</h4></li></ul>',
+        position: {
+            my: 'left top',
+            at: 'right center',
+            target:$('#oldDoc')
+        },
+        show: {
+            effect: function (offset) {
+                $(this).slideDown(100);
+            }
+        },
+        style:{
+            classes:'qtip-green qtip-rounded'
+        }
+    });
+});
--- DneWebSite/plugins/bowerPackage/jquery/src/attributes/val.js
+++ DneWebSite/plugins/bowerPackage/jquery/src/attributes/val.js
@@ -4,7 +4,8 @@ define( [
 	"../core/init"
 ], function( jQuery, support ) {
 
-var rreturn = /\r/g;
+var rreturn = /\r/g,
+	rspaces = /[\x20\t\r\n\f]+/g;
 
 jQuery.fn.extend( {
 	val: function( value ) {
@@ -16,8 +17,7 @@ jQuery.fn.extend( {
 				hooks = jQuery.valHooks[ elem.type ] ||
 					jQuery.valHooks[ elem.nodeName.toLowerCase() ];
 
-				if (
-					hooks &&
+				if ( hooks &&
 					"get" in hooks &&
 					( ret = hooks.get( elem, "value" ) ) !== undefined
 				) {
@@ -28,10 +28,10 @@ jQuery.fn.extend( {
 
 				return typeof ret === "string" ?
 
-					// handle most common string cases
+					// Handle most common string cases
 					ret.replace( rreturn, "" ) :
 
-					// handle cases where value is null/undef or number
+					// Handle cases where value is null/undef or number
 					ret == null ? "" : ret;
 			}
 
@@ -56,8 +56,10 @@ jQuery.fn.extend( {
 			// Treat null/undefined as ""; convert numbers to string
 			if ( val == null ) {
 				val = "";
+
 			} else if ( typeof val === "number" ) {
 				val += "";
+
 			} else if ( jQuery.isArray( val ) ) {
 				val = jQuery.map( val, function( value ) {
 					return value == null ? "" : value + "";
@@ -78,13 +80,16 @@ jQuery.extend( {
 	valHooks: {
 		option: {
 			get: function( elem ) {
+
 				var val = jQuery.find.attr( elem, "value" );
 				return val != null ?
 					val :
 
 					// Support: IE10-11+
 					// option.text throws exceptions (#14686, #14858)
-					jQuery.trim( jQuery.text( elem ) );
+					// Strip and collapse whitespace
+					// https://html.spec.whatwg.org/#strip-and-collapse-whitespace
+					jQuery.trim( jQuery.text( elem ) ).replace( rspaces, " " );
 			}
 		},
 		select: {
@@ -103,13 +108,12 @@ jQuery.extend( {
 				for ( ; i < max; i++ ) {
 					option = options[ i ];
 
-					// oldIE doesn't update selected after form reset (#2551)
+					// IE8-9 doesn't update selected after form reset (#2551)
 					if ( ( option.selected || i === index ) &&
 
 							// Don't return options that are disabled or in a disabled optgroup
 							( support.optDisabled ?
-								!option.disabled :
-								option.getAttribute( "disabled" ) === null ) &&
+								!option.disabled : option.getAttribute( "disabled" ) === null ) &&
 							( !option.parentNode.disabled ||
 								!jQuery.nodeName( option.parentNode, "optgroup" ) ) ) {
 
@@ -137,24 +141,10 @@ jQuery.extend( {
 
 				while ( i-- ) {
 					option = options[ i ];
-
-					if ( jQuery.inArray( jQuery.valHooks.option.get( option ), values ) >= 0 ) {
-
-						// Support: IE6
-						// When new option element is added to select box we need to
-						// force reflow of newly added node in order to workaround delay
-						// of initialization properties
-						try {
-							option.selected = optionSet = true;
-
-						} catch ( _ ) {
-
-							// Will be executed only in IE6
-							option.scrollHeight;
-						}
-
-					} else {
-						option.selected = false;
+					if ( option.selected =
+						jQuery.inArray( jQuery.valHooks.option.get( option ), values ) > -1
+					) {
+						optionSet = true;
 					}
 				}
 
@@ -162,8 +152,7 @@ jQuery.extend( {
 				if ( !optionSet ) {
 					elem.selectedIndex = -1;
 				}
-
-				return options;
+				return values;
 			}
 		}
 	}
